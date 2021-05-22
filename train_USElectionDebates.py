fc21f9778bc5706f5627f78d4ccd1d630d303fa1
Daniel Hu <daniel@fileread.com>
2021-05-21 19:00:30 -0700

    work

--- train_USElectionDebates.py
+++ train_USElectionDebates.py
@@ -135,6 +135,7 @@ def single_train(max_seq_length=512,
         warmup_epoch_count=warmup_epoch_count,
         total_epoch_count=max_epochs)
     # train model
+    log_dir = 'gs://sentargcdd/train_0521'
     history = model.fit(x=train_X,
                         y=train_Y,
                         validation_split=0.15,
